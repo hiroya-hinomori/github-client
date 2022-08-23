cf13d0d5068cbac7bb9d778521b165c74c167bfe
hiroya-hinomori <hiroya.hinomori@kinto-technologies.com>
2022-08-23 13:15:52 +0900

    Add: project

---
+++ GithubClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubClient
+//
+//  Created by 日野森 寛也（Hiroya Hinomori） on 2022/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubClient/GithubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubClientApp.swift
+//  GithubClient
+//
+//  Created by 日野森 寛也（Hiroya Hinomori） on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
